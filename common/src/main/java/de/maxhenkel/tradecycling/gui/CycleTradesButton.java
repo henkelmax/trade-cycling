ceb60bb1d86a35bdef14dc4774360c31e94f6430
Max Henkel <max@maxhenkel.de>
2023-06-05 16:28:49 +0200

    Prepare for 1.20

--- common/src/main/java/de/maxhenkel/tradecycling/gui/CycleTradesButton.java
+++ common/src/main/java/de/maxhenkel/tradecycling/gui/CycleTradesButton.java
@@ -1,9 +1,10 @@
 package de.maxhenkel.tradecycling.gui;
 
 import com.mojang.blaze3d.systems.RenderSystem;
-import com.mojang.blaze3d.vertex.PoseStack;
 import de.maxhenkel.tradecycling.TradeCyclingMod;
 import de.maxhenkel.tradecycling.mixin.MerchantMenuAccessor;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.gui.components.AbstractButton;
 import net.minecraft.client.gui.narration.NarrationElementOutput;
 import net.minecraft.client.gui.screens.inventory.MerchantScreen;
@@ -32,21 +33,21 @@ public class CycleTradesButton extends AbstractButton {
     }
 
     @Override
-    public void render(PoseStack poseStack, int mouseX, int mouseY, float partialTicks) {
+    public void render(GuiGraphics guiGraphics, int mouseX, int mouseY, float partialTicks) {
         visible = canCycle(screen.getMenu());
-        super.render(poseStack, mouseX, mouseY, partialTicks);
+        super.render(guiGraphics, mouseX, mouseY, partialTicks);
     }
 
     @Override
-    public void renderWidget(PoseStack poseStack, int mouseX, int mouseY, float partialTicks) {
+    public void renderWidget(GuiGraphics guiGraphics, int mouseX, int mouseY, float partialTicks) {
         RenderSystem.setShader(GameRenderer::getPositionTexShader);
         RenderSystem.setShaderColor(1F, 1F, 1F, 1F);
         RenderSystem.setShaderTexture(0, ARROW_BUTTON);
         if (isHovered) {
-            blit(poseStack, getX(), getY(), 0, 14, WIDTH, HEIGHT, 32, 32);
-            screen.renderTooltip(poseStack, Collections.singletonList(Component.translatable("tooltip.trade_cycling.cycle_trades").getVisualOrderText()), mouseX, mouseY);
+            guiGraphics.blit(ARROW_BUTTON, getX(), getY(), 0, 14, WIDTH, HEIGHT, 32, 32);
+            guiGraphics.renderTooltip(Minecraft.getInstance().font, Collections.singletonList(Component.translatable("tooltip.trade_cycling.cycle_trades").getVisualOrderText()), mouseX, mouseY);
         } else {
-            blit(poseStack, getX(), getY(), 0, 0, WIDTH, HEIGHT, 32, 32);
+            guiGraphics.blit(ARROW_BUTTON, getX(), getY(), 0, 0, WIDTH, HEIGHT, 32, 32);
         }
     }
 
