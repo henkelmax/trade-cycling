f4b0e982a062dae8201291acb218581775d32aa8
Max Henkel <max@maxhenkel.de>
2026-03-25 07:16:18 +0100

    Update forge to 21.6

--- forge/src/main/java/de/maxhenkel/tradecycling/ForgeTradeCyclingMod.java
+++ forge/src/main/java/de/maxhenkel/tradecycling/ForgeTradeCyclingMod.java
@@ -46,6 +46,6 @@ public class ForgeTradeCyclingMod extends TradeCyclingMod {
 
     @Override
     public boolean isModLoaded(String modId) {
-        return ModList.get().isLoaded(modId);
+        return ModList.isLoaded(modId);
     }
 }
